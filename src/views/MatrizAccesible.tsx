4ad0db223d64246c08f21d7f4377b1fdfee551ff
Irving-8man <geyler0502@outlook.com>
2025-02-15 07:55:23 -0600

    fix(estilos): evitar que los nombres de tokens largos se desborden

--- src/views/MatrizAccesible.tsx
+++ src/views/MatrizAccesible.tsx
@@ -69,9 +69,12 @@ export default function MatrizAccesible() {
                             {combinaciones.map((fila, index) => (
                                 <li key={index} className="border p-4 rounded-md bg-gray-100 my-14">
 
-                                    <p className="font-semibold text-lg inline-flex gap-2 items-center mb-3">
-                                        Fondo: {fila[0].color1.nombre} <span className="w-6 h-6 block border border-black" style={{ backgroundColor: fila[0].color1.hex }}></span>
+
+                                    <p className="font-semibold text-lg inline-flex gap-2 items-center mb-3 ">
+                                        <span className="token-name2"> Fondo: {fila[0].color1.nombre}</span> <span className="w-6 h-6 block border border-black" style={{ backgroundColor: fila[0].color1.hex }}></span>
                                     </p>
+
+
                                     <div className="mt-2 grid grid-cols-2 gap-2">
                                         {fila.map((combinacion) => (
                                             <PiezaCombinacion combinacion={combinacion} key={combinacion.id} />
