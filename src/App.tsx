d8eadc1084ae0aa90425b52d59f42456d1e3b551
Irving-8man <geyler0502@outlook.com>
2025-02-17 07:42:27 -0600

    update: agregado página NotFound-404

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Route } from "wouter";
+import { Route, Switch } from "wouter";
 import { Toaster } from "sonner";
 import Header from "./components/sections/Header";
 import Footer from "./components/sections/Footer";
 import HomePage from "./views/HomePage";
 import MatrizAccesible from "./views/MatrizAccesible";
+import NotFound from "./views/NotFound";
 import "./index.css";
 
 export default function App() {
@@ -13,8 +14,11 @@ export default function App() {
             <div className="min-h-screen flex flex-col justify-between">
                 <div>
                     <Header />
-                    <Route path="/" component={HomePage} />
-                    <Route path="/matriz-accesibilidad/" component={MatrizAccesible} />
+                    <Switch>
+                        <Route path="/" component={HomePage} />
+                        <Route path="/matriz-accesibilidad/" component={MatrizAccesible} />
+                        <Route component={NotFound} /> {/* Captura rutas no existentes */}
+                    </Switch>
                 </div>
                 <Footer />
             </div>
---
+++ src/views/NotFound.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+    return (
+        <main className="min-h-[60vh] grid place-content-center">
+            <section className="text-center">
+                <h1 className="text-6xl md:text-8xl font-bold">404</h1>
+                <p className="text-lg mt-2">Página no encontrada</p>
+            </section>
+        </main>
+    )
+}
